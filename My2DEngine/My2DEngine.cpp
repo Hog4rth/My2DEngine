90674f75cb2ad0f62a8f328e9fc86b4d298254fd
Hog4rth <apirosso@gmail.com>
2026-08-22 17:51:47 +0200

    refactor: improve engine initialization error message

--- My2DEngine/My2DEngine.cpp
+++ My2DEngine/My2DEngine.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     GameEngine engine;
 
     if (!engine.Initialize()) {
-        std::cerr << "GPU can't start up\n";
+        std::cerr << "Engine failed to initialize\n";
         return -1;
     }
 
