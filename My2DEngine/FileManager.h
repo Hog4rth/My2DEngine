79748f2d6f24daa6312a0a54d25ae4c0bcc95997
Hog4rth <apirosso@gmail.com>
2026-08-22 19:24:55 +0200

    refactor(filemanager): optimize Load method using std::string_view and transparent map lookup

--- My2DEngine/FileManager.h
+++ My2DEngine/FileManager.h
@@ -4,36 +4,36 @@
 
 class FileManager {
 private:
-	std::map<std::string, std::string> defaultSettings;
+	std::map<std::string, std::string, std::less<>> defaultSettings;
 	const std::string filePath = "Settings.ini";
 
 public:
 	void LoadDefaultSettings();
 
 	template <typename T>
-	T Load(const std::string& key, const T defaultValue) {
+	T Load(std::string_view key, const T defaultValue) {
 
-		if (defaultSettings.find(key) != defaultSettings.end()) {
-
-			std::string keyFound = defaultSettings[key];
-
-			if constexpr (std::is_same_v<T, std::string>) {
-				return keyFound;
-
-			}
-			else {
-				T valueFound;
-				std::stringstream Convert(keyFound);
-				Convert >> valueFound;
-
-				if (Convert.fail()) {
-					std::cerr << "Warning: Failed to convert value for key '" << key << "'. Using default value.\n";
-					return defaultValue;
-				}
-
-				return valueFound;
-			}
+		auto it = defaultSettings.find(key);
+		if (it == defaultSettings.end()) {
+			return defaultValue;
 		}
-		return defaultValue;
+
+		const std::string keyFound = it->second;
+
+		if constexpr (std::is_same_v<T, std::string>) {
+			return keyFound;
+		}
+		else {
+			T valueFound;
+			std::stringstream Convert(keyFound);
+			Convert >> valueFound;
+
+			if (Convert.fail()) {
+				std::cerr << "Warning: Failed to convert value for key '" << key << "'. Using default value.\n";
+				return defaultValue;
+			}
+
+			return valueFound;
+		}
 	}
 };
