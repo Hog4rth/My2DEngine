30d256a8682ed0431fb1b5f2ae212f320e14fdd2
Hog4rth <apirosso@gmail.com>
2026-08-23 16:16:55 +0200

    fix: clean the default settings from " \t\r\n"

--- My2DEngine/FileManager.cpp
+++ My2DEngine/FileManager.cpp
@@ -11,12 +11,21 @@ void FileManager::LoadDefaultSettings() {
 	if (file.is_open()) {
 		std::string line;
 
+		auto trim = [](std::string& str) {
+			str.erase(0, str.find_first_not_of(" \t\r\n"));
+			str.erase(str.find_last_not_of(" \t\r\n") + 1);
+		};
+
 		while (std::getline(file, line)) {
 			size_t equalPosition = line.find('=');
 
 			if (equalPosition != std::string::npos) {
 				std::string key = line.substr(0, equalPosition);
 				std::string value = line.substr(++equalPosition);
+
+				trim(key);
+				trim(value);
+
 				defaultSettings[key] = value;
 			}
 		}
