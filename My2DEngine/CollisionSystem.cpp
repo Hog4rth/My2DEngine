49920f8691caba282578979f85c3192cdcd9a995
Hog4rth <apirosso@gmail.com>
2026-08-23 11:28:05 +0200

    refactor(collision): introduce Rect struct for cleaner AABB overlap checks

--- My2DEngine/CollisionSystem.cpp
+++ My2DEngine/CollisionSystem.cpp
@@ -1,5 +1,15 @@
 #include "CollisionSystem.h"
 
+struct Rect {
+	float x, y, w, h;
+	float Right() const { return x + w; }
+	float Bottom() const { return y + h; }
+
+	bool Overlaps(const Rect& o) const {
+		return Right() > o.x && o.Right() > x && Bottom() > o.y && o.Bottom() > y;
+	}
+};
+
 void CollisionSystem::UpdateCollisions(std::span<const TagComponent> tags, std::span<const PositionComponent> positions, std::span<const SizeComponent> sizes, std::span<CollisionComponent> colliders, std::span<VelocityComponent> velocities, const float deltaTime) {
 
 	for (size_t i = 0; i < tags.size(); ++i) {
@@ -24,16 +34,16 @@ void CollisionSystem::UpdateCollisions(std::span<const TagComponent> tags, std::
 
 			float futurePositionX = positions[i].x + velocities[i].velocityX * deltaTime;
 
-			if (futurePositionX + sizes[i].width > positions[j].x &&
-				positions[j].x + sizes[j].width > futurePositionX &&
-				positions[i].y + sizes[i].height > positions[j].y &&
-				positions[j].y + sizes[j].height > positions[i].y) { // if the future position.x and the current position.y of i-object is colliding with j-object
+			Rect rectI_X = { futurePositionX, positions[i].y, sizes[i].width, sizes[i].height };
+			Rect rectJ = { positions[j].x, positions[j].y, sizes[j].width, sizes[j].height };
 
-				if (velocities[i].velocityX > 0) { // and if i-object is moving right
+			if (rectI_X.Overlaps(rectJ)) {
+
+				if (velocities[i].velocityX > 0) { // if i-object is moving right
 					velocities[i].velocityX = (positions[j].x - positions[i].x - sizes[i].width) / deltaTime; // adjust the velocity to stop at the left edge of j-object
 					colliders[i].onTheRightWall = true;
 				}
-				else if (velocities[i].velocityX < 0) { // and if i-object is moving left
+				else if (velocities[i].velocityX < 0) { // if i-object is moving left
 					velocities[i].velocityX = (positions[j].x + sizes[j].width - positions[i].x) / deltaTime; // adjust the velocity to stop at the right edge of j-object
 					colliders[i].onTheLeftWall = true;
 				}
@@ -42,11 +52,9 @@ void CollisionSystem::UpdateCollisions(std::span<const TagComponent> tags, std::
 			futurePositionX = positions[i].x + velocities[i].velocityX * deltaTime; // Calculate again because velocity may have changed
 			float futurePositionY = positions[i].y + velocities[i].velocityY * deltaTime;
 
-			if (futurePositionX + sizes[i].width > positions[j].x &&
-				positions[j].x + sizes[j].width > futurePositionX &&
-				futurePositionY + sizes[i].height > positions[j].y &&
-				positions[j].y + sizes[j].height > futurePositionY) { // if the future position.x and the future position.y of i-object is colliding with j-object
-				// Note: Now we can use future position.y because we have already adjusted the velocity.x if there was a collision in the previous step
+			Rect rectI_Y = { futurePositionX, futurePositionY, sizes[i].width, sizes[i].height };
+
+			if (rectI_Y.Overlaps(rectJ)) { // Note: Now we can use future position.y because we have already adjusted the velocity.x if there was a collision in the previous step
 
 				if (velocities[i].velocityY > 0) { // and if i-object is moving down
 					velocities[i].velocityY = (positions[j].y - positions[i].y - sizes[i].height) / deltaTime; // adjust the velocity to stop at the top edge of j-object
