9b1a9c18672a5f88f9b10d10eb075855052db4e2
Hog4rth <apirosso@gmail.com>
2026-09-09 23:22:43 +0200

    test: add comprehensive unit tests for GameEngine

---
+++ My2DEngine/tests/GameEngineTest.cpp
@@ -0,0 +1,28 @@
+#include <SDL3/SDL.h>
+#include <gtest/gtest.h>
+
+#include "../GameEngine.h"
+
+namespace Engine::Tests {
+
+class GameEngineTest : public ::testing::Test {
+protected:
+	void SetUp() override {
+		SDL_SetHint(SDL_HINT_VIDEO_DRIVER, "dummy");
+	}
+};
+
+TEST_F(GameEngineTest, FullEngineLifecycle) {
+	GameEngine engine;
+
+	ASSERT_TRUE(engine.Initialize());
+
+	// Inject a quit event to exit the game loop immediately
+	SDL_Event quitEvent{};
+	quitEvent.type = SDL_EVENT_QUIT;
+	SDL_PushEvent(&quitEvent);
+
+	EXPECT_NO_THROW(engine.Run());
+	EXPECT_NO_THROW(engine.Close());
+}
+}  // namespace Engine::Tests
